d0ec22f60ef91018ab3cf82a95758827d7d6a7c4
Rob Crouthamel <rob@farmjenny.com>
2020-05-21 12:30:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fj_farmjennycellular',
-    version='0.2.4',
+    version='0.2.5',
     author='Rob Crouthamel',
     author_email='rob@farmjenny.com',
     description='farm jenny cellular python libraries',
