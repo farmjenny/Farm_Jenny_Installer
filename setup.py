bcdf6ec5f555ee1874903677baca6322434ec9eb
Rob Crouthamel <rob.crouthamel@gmail.com>
2020-06-25 09:06:09 -0400

    Bump python library rev for testing Rv1 HAT

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fj_farmjennycellular',
-    version='0.2.7',
+    version='0.3.0',
     author='Rob Crouthamel',
     author_email='rob@farmjenny.com',
     description='farm jenny cellular python libraries',
