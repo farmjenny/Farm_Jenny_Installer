0d09d152b9db0880b23bccb8a8624f35a52149e8
Rob Crouthamel <rob.crouthamel@gmail.com>
2020-06-29 11:37:29 -0400

    Reordered prompts, added color

--- installer/install.sh
+++ installer/install.sh
@@ -1,90 +1,103 @@
 #!/bin/sh
 
-echo "Select the Farm Jenny hardware to install:"
-echo "1: LTE Border Router HAT"
-echo "2: Other"
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+SET='\033[0m'
 
-read hardware
-case $hardware in
-    1)    echo "You selected LTE Border Router HAT";;
-    2)    echo "You selected Other";;
-    *)    echo "Sorry, I don't understand. Bye!"; exit 1;
-esac
-
-# Install git
-echo "Installing git and python-setuptools"
+# Bootstrapping ----------------------------------------------------------------
+echo "${YELLOW}Preparing for installation${SET}"
 sudo apt-get --assume-yes install git python3-setuptools python3-pip python3-RPi.GPIO ppp 
 
-if [ $hardware -eq 1 ];	then
-	echo "What cellular modem is installed in the HAT?:"
-	echo "1: Nimbelink NL-SW-LTE-QBG96 (Quectel BG96)"
-	echo "2: Other modem"
-	echo "3: None"
-	
-	read modem
-	case $modem in
-		1)    echo "You selected Nimbelink NL-SW-LTE-QBG96, configuring for LTE-M with 2G Fallback"
-				EXTRA='';;
-		2)    echo "You selected Other modem, no extended settings to apply."
-				EXTRA='';;
-		3)    echo "You indicated no cellular modem installed, skipping modem config -- rerun this installer if a modem is added later."
-				EXTRA='';;
-		*) 	  echo "Sorry, I don't understand. Bye!"; exit 1;
-	esac
-fi
-
-if [ $hardware -eq 1 ];	then
-	echo "Installing Cellular Support"
-	case $modem in
-		1)    echo "Installing Farm Jenny Libraries for HAT with BG96-based modem"
-				git clone https://github.com/farmjenny/Farm_Jenny_Installer.git
-				cd Farm_Jenny_Installer
-				sudo python3 setup.py install
-				;;
-		2)    echo "No libraries to install.";;
-		3)    echo "No libraries to install.";;
-		*)    echo "Sorry, I don't understand. Bye!"; exit 1;
-	esac
-fi
-
-echo "Downloading chatscript templates"
+echo "${YELLOW}Downloading chatscript templates${SET}"
 wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/ppp/chat-connect -O chat-connect
 
 if [ $? -ne 0 ]; then
-    echo "Download failed"
+    echo "${RED}Download failed${SET}"
     exit 1; 
 fi
 
 wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/ppp/chat-disconnect -O chat-disconnect
 
 if [ $? -ne 0 ]; then
-    echo "Download failed"
+    echo "${RED}Download failed${SET}"
     exit 1;
 fi
 
 wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/ppp/provider -O provider
 
 if [ $? -ne 0 ]; then
-    echo "Download failed"
+    echo "${RED}Download failed${SET}"
     exit 1;
 fi
 
-echo "What is your carrier's or MVNO's APN? (e.g., hologram)"
+# User input
+
+echo "${YELLOW}Select the Farm Jenny hardware to install:${SET}"
+echo "${YELLOW}1: LTE Border Router HAT${SET}"
+echo "${YELLOW}2: Other${SET}"
+
+read hardware
+case $hardware in
+    1)    echo "${YELLOW}You selected LTE Border Router HAT${SET}";;
+    2)    echo "${YELLOW}You selected Other${SET}";;
+    *)    echo "${RED}Sorry, I don't understand. Bye!${SET}"; exit 1;
+esac
+
+if [ $hardware -eq 1 ];	then
+
+	devicepath= "ttyUSB3"
+
+	echo "${YELLOW}What cellular modem is installed in the HAT?:${SET}"
+	echo "${YELLOW}1: Nimbelink NL-SW-LTE-QBG96 (Quectel BG96)${SET}"
+	echo "${YELLOW}2: Other modem${SET}"
+	echo "${YELLOW}3: None${SET}"
+	
+	read modem
+	case $modem in
+		1)    echo "${YELLOW}You selected Nimbelink NL-SW-LTE-QBG96, configuring for LTE-M with 2G Fallback${SET}"
+				devicepath= "ttyUSB3"
+				EXTRA='';;
+		2)    echo "${YELLOW}You selected Other modem, no extended settings to apply.${SET}"
+				devicepath= "ttyUSB3"
+				EXTRA='';;
+		3)    echo "${YELLOW}You indicated no cellular modem installed, skipping modem config -- rerun this installer if a modem is added later.${SET}"
+				devicepath= "ttyUSB3"
+				EXTRA='';;
+		*) 	  echo "${RED}Sorry, I don't understand. Bye!${SET}"; exit 1;
+	esac
+fi
+
+if [ $hardware -eq 1 ];	then
+	echo "${YELLOW}Installing Cellular Support${SET}"
+	case $modem in
+		1)    echo "${YELLOW}Installing Farm Jenny Libraries for HAT with BG96-based modem${SET}"
+				git clone https://github.com/farmjenny/Farm_Jenny_Installer.git
+				cd Farm_Jenny_Installer
+				sudo python3 setup.py install
+				;;
+		2)    echo "${YELLOW}No libraries to install.${SET}";;
+		3)    echo "${YELLOW}No libraries to install.${SET}";;
+		*)    echo "${RED}Sorry, I don't understand. Bye!${SET}"; exit 1;
+	esac
+fi
+
+echo "${YELLOW}What is your carrier's or MVNO's APN? (e.g., hologram)${SET}"
 read carrierapn 
 
 while [ 1 ]
 do
-	echo "Does your carrier need username and password? [Y/n]"
+	echo "${YELLOW}Does your carrier need username and password? [Y/n]${SET}"
 	read usernpass
 	
 	case $usernpass in
 		[Yy]* )  while [ 1 ] 
         do 
         
-        echo "Enter username"
+        echo "${YELLOW}Enter username${SET}"
         read username
 
-        echo "Enter password"
+        echo "${YELLOW}Enter password${SET}"
         read password
         sed -i "s/noauth/#noauth\nuser \"$username\"\npassword \"$password\"/" provider
         break 
@@ -93,12 +106,9 @@ do
         break;;
 		
 		[Nn]* )  break;;
-		*)  echo "Please select one of: Y, y, N, or n";;
+		*)  echo "${RED}Please select one of: Y, y, N, or n${SET}";;
 	esac
 done
-
-echo "What is your device communication PORT? (typically: ttyUSB3)"
-read devicepath 
 
 mkdir -p /etc/chatscripts
 sed -i "/#EXTRA/d" chat-connect
@@ -117,28 +127,28 @@ if ! (grep -q 'sudo route' /etc/ppp/ip-up ); then
 fi
 
 if [ $hardware -eq 1 ];	then
-	echo "Your HAT can operate as a Thread Border Router.  Install OTBR? (WARNING: this will take awhile) [Y/n]"
+	echo "${YELLOW}Your HAT can operate as a Thread Border Router.  Install OTBR? (WARNING: this will take awhile) [Y/n]${SET}"
 	read otbrinstall
 	
 	case $otbrinstall in
 		[Yy]* )
         	# Install OTBR
-		echo "downloading OTBR"
+		echo "${YELLOW}downloading OTBR${SET}"
         	sudo git clone  https://github.com/farmjenny/borderrouter.git
 		cd borderrouter
 		
-		echo "installing OTBR dependencies"
+		echo "${YELLOW}installing OTBR dependencies${SET}"
 		sudo ./script/bootstrap
 		
-		echo "Building OTBR ***without*** Access Point"
+		echo "${YELLOW}Building OTBR ***without*** Access Point${SET}"
 		sudo NETWORK_MANAGER=0 ./script/setup
-		echo "Finished installing OTBR."
+		echo "${YELLOW}Finished installing OTBR.${SET}"
 		cd ..
 		
 		# Install OpenThread Stack for RCP
-		echo "Need OT Posix App for RCP"
+		echo "${YELLOW}Need OT Posix App for RCP${SET}"
 		
-		echo "downloading OT"
+		echo "${YELLOW}downloading OT${SET}"
 		sudo git clone https://github.com/openthread/openthread
 		cd openthread
 		sudo git checkout tags/thread-reference-20191113
@@ -147,15 +157,15 @@ if [ $hardware -eq 1 ];	then
 		sudo make -f src/posix/Makefile-posix
 		
 		# Move ot-ncp to proper location
-		echo "moving ot-ncp to /usr/bin"
+		echo "${YELLOW}moving ot-ncp to /usr/bin${SET}"
 		sudo cp /output/posix/armv7l-unknown-linux-gnueabihf/bin/* /usr/bin/
 		cd ..
 		
 		# Configure GPIO for INT and RESET at powerup (before wpantund starts)
-		echo "Configuring gpio pins at startup"
+		echo "${YELLOW}Configuring gpio pins at startup${SET}"
 		wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/util/farmjenny_gpio.sh -O farmjenny_gpio.sh
 		if [ $? -ne 0 ]; then
-    		echo "Download failed"
+    		echo "${RED}Download failed${SET}"
     		exit 1;
 		fi
 		# copy file to correct location
@@ -167,7 +177,7 @@ if [ $hardware -eq 1 ];	then
 		# add the farmjenny_gpio service
 		wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/util/farmjenny_gpio.service -O farmjenny_gpio.service
 		if [ $? -ne 0 ]; then
-    		echo "Download failed"
+    		echo "${RED}Download failed${SET}"
     		exit 1;
 		fi
 		sudo mv farmjenny_gpio.service /lib/systemd/system/
@@ -180,10 +190,10 @@ if [ $hardware -eq 1 ];	then
 		sudo systemctl enable wpantund.service
 
 		# reconfigure wpantund for RCP
-		echo "Configuring wpantund to use RCP with INT and RESET"
+		echo "${YELLOW}Configuring wpantund to use RCP with INT and RESET${SET}"
 		wget --no-check-certificate  https://raw.githubusercontent.com/farmjenny/Farm_Jenny_Installer/master/installer/util/wpantund.conf.rcp -O wpantund.conf.rcp
 		if [ $? -ne 0 ]; then
-    		echo "Download failed"
+    		echo "${RED}Download failed${SET}"
     		exit 1;
 		fi
 		# save a copy of the existing wpantund configuration
@@ -192,9 +202,10 @@ if [ $hardware -eq 1 ];	then
 		sudo mv wpantund.conf.rcp /etc/wpantund.conf
 		;;
 		[Nn]* )  break;;
-		*)  echo "Please select one of: Y, y, N, or n";;
+		*)  echo "${RED}Please select one of: Y, y, N, or n${SET}";;
 	esac
 fi
 
-read -p "Farm Jenny installation is complete, press ENTER key to reboot and start your device" ENTER
+echo "${YELLOW}Farm Jenny installation is complete.  Use ${BLUE}\"sudo pon\"${YELLOW} to connect and ${BLUE}\"sudo poff\"${YELLOW} to disconnect.${SET}" 
+read -p "${YELLOW}Farm Jenny installation is complete, press ENTER key to reboot and start your device${SET}" ENTER
 reboot
