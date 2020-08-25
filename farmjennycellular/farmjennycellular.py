cc9a9cd081a46affa15ab887e15c0bb1e96c701f
Rob Crouthamel <rob.crouthamel@gmail.com>
2020-08-25 16:33:44 -0400

    BG96: Add support for active GNSS antenna

--- farmjennycellular/farmjennycellular.py
+++ farmjennycellular/farmjennycellular.py
@@ -432,16 +432,30 @@ class FarmJennyHatBg96:
 		self.sendATComm(self.CTRL_Z,"OK",8) # with 8 seconds timeout
 		
 	#******************************************************************************************
-	#*** GNSS Functions ***********************************************************************
+	#*** BG96 GNSS Functions ******************************************************************
 	#******************************************************************************************
 
-	# Function for turning on GNSS
-	def turnOnGNSS(self):
-		self.sendATComm("AT+QGPS=1","OK\r\n")
+	# Function for turning on GNSS (and enable active antenna if so equipped)
+	def turnOnGNSS(self,active_ant = False):
+		if(active_ant == True):
+			#apply DC power to antenna
+			node.sendATComm("ATE0","OK\r\n")
+			node.sendATComm("AT+QCFG=\"gpio\",1,64,1,0,0,1","OK\r\n")
+			node.sendATComm("AT+QCFG=\"gpio\",3,64,1,1","OK\r\n")
 
-	# Function for turning of GNSS
-	def turnOffGNSS(self):
+	# Function for turning of GNSS (and turn off active antenna if so equipped)
+	def turnOffGNSS(self,active_ant = False):
+		if(active_ant == True):
+			#remove DC power to antenna to save power
+			node.sendATComm("ATE0","OK\r\n")
+			node.sendATComm("AT+QCFG=\"gpio\",1,64,1,0,0,1","OK\r\n")
+			node.sendATComm("AT+QCFG=\"gpio\",3,64,0,1","OK\r\n")
+			
+		self.sendATComm("AT+QGPSEND","OK\r\n")		
+		# Note: AT+QGPS=1 fails if GPS is already enabled, so to be safe, always issue QGSPEND first
 		self.sendATComm("AT+QGPSEND","OK\r\n")
+		delay(1000)
+		self.sendATComm("AT+QGPS=1","OK\r\n")
 	
 	# Function for getting latitude
 	def getLatitude(self):
