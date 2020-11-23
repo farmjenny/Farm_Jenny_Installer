9b44892cec2238ad1718dfda237af5ae9e6b36d7
Rob Crouthamel <rob.crouthamel@gmail.com>
2020-11-23 14:13:29 -0500

    Update otbr-posix commit Start gpio service earlier Add buttonledtest.py to util

--- installer/install.sh
+++ installer/install.sh
@@ -339,7 +339,7 @@ if [ $hardware -eq 1 ];	then
         	sudo git clone  https://github.com/openthread/ot-br-posix.git 2>&1 | tee -a /home/pi/farmjenny/logs/install.log
 		cd ot-br-posix
 		# check out a version ot OTBR we have tested
-		git checkout ea9cd69 2>&1 | tee -a /home/pi/farmjenny/logs/install.log
+		git checkout 6d49606 2>&1 | tee -a /home/pi/farmjenny/logs/install.log
 		
 		echo "${YELLOW}Installing OTBR dependencies${SET}"
 		sudo ./script/bootstrap 2>&1 | tee -a /home/pi/farmjenny/logs/install.log
---
+++ installer/util/bg96/buttonledtest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from farmjennycellular import farmjennycellular
+import time
+import RPi.GPIO as GPIO
+# tests operation if the user button by illuminating the user led when pressed
+node = farmjennycellular.FarmJennyHatBg96(serial_port="/dev/ttyUSB2")
+# Hello!
+GPIO.setwarnings(False)
+node.setupGPIO()
+node.turnOffUserLED()
+# User button is active low
+print ('Testing User Button and LED.  Green LED should light while Button is pressed.  Use <CTL>-C to exit.')
+while 1:
+        if node.readUserButton():
+                node.turnOffUserLED()
+        else:
+                node.turnOnUserLED()
+
